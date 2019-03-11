288ba9b2afa9e3b546127683d41fe64727fba3bc
Rich Lane <richlanedesign@gmail.com>
2019-03-11 00:13:21 -0600

    Updates

---
+++ precache-manifest.1bb17cd383b877a5b464f42ddfe7692c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3fe81c4c31232682d87b",
+    "url": "/clickyGame/static/js/runtime~main.3fe81c4c.js"
+  },
+  {
+    "revision": "07db8ffb01e20ae47221",
+    "url": "/clickyGame/static/js/main.07db8ffb.chunk.js"
+  },
+  {
+    "revision": "00ec0959a47ed983999e",
+    "url": "/clickyGame/static/js/2.00ec0959.chunk.js"
+  },
+  {
+    "revision": "07db8ffb01e20ae47221",
+    "url": "/clickyGame/static/css/main.4232086c.chunk.css"
+  },
+  {
+    "revision": "fa670113b6eebed923117fca046db7f2",
+    "url": "/clickyGame/index.html"
+  }
+];
